7523bb0b11ca027637c2292349c22d6e538a1d14
Herman <german1514u@gmail.com>
2024-12-18 13:57:20 +0100

    HomeWork28

---
+++ src/HomeWork28/Task_02/PersonTest.java
@@ -0,0 +1,109 @@
+package HomeWork28.Task_02;
+
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+
+
+
+public class PersonTest {
+
+    Person person;
+    String startEmail = "john@test.com";
+    String startPassword = "qwerty1Q$";
+
+    @BeforeEach
+    void setUp(){
+        person = new Person(startEmail, startPassword);
+    }
+
+    @Test
+    void testValidEmailSet() {
+        String validEmail = "valid@test.com";
+
+        person.setEmail(validEmail);
+
+        assertEquals(validEmail, person.getEmail());
+        assertNotEquals(startEmail, person.getEmail());
+    }
+
+    /*
+    1. Берем невалидный email
+    2. Устанавливаем его сеттером персону
+    3. Ожидаемый результат: invalidEmail установлен не будет
+    4.
+     */
+
+    // testmail.net
+    // test@@mail.net
+
+    @ParameterizedTest
+    @MethodSource("invalidEmailData")
+    void testInvalidEmailSet(String invalidEmail) {
+        person.setEmail(invalidEmail);
+        assertNotEquals(invalidEmail, person.getEmail());
+        assertEquals(startEmail, person.getEmail());
+    }
+
+    static Stream<String> invalidEmailData() {
+        return Stream.of(
+                "testmail.net",
+                "test@@mail.net",
+                "test@mai@l.net",
+                "test@mailnet",
+                "test@mail.net.",
+                "test@mailne.t",
+                "test@ mail.net",
+                "test@ma!il.net",
+                "t#est@mail.net",
+                "1test@mail.net",
+                "_test@mail.net",
+                ".test@mail.net",
+                "test+1@mail.net"
+        );
+    }
+//////////////////////////////////////////////////HOMEWORK TASK 2///////////////////////////////////////////////////////////////////////////////////
+    @Test
+    void testValidPasswordSet() {
+        String validPassword = "qwerty1Q$";
+
+        person.setPassword(validPassword);
+
+        assertEquals(validPassword, person.getPassword());
+    }
+
+
+
+    @ParameterizedTest
+    @MethodSource("invalidPasswordData")
+    void testInvalidPasswordSet(String invalidPassword) {
+
+        person.setPassword(invalidPassword);
+
+        assertEquals(startPassword, person.getPassword());
+        assertNotEquals(invalidPassword, person.getPassword());
+    }
+
+
+    static Stream<String> invalidPasswordData() {
+        return Stream.of(
+                "Vallied1234",
+                "voi$",
+                "qwe$",
+                "Q$",
+                "qwQ$",
+                "q",
+                "$",
+                "",
+                "job87"
+        );
+
+    }
+}
---
+++ src/lesson_28/PersonApp.java
@@ -0,0 +1,17 @@
+package lesson_28;
+
+public class PersonApp {
+    public static void main(String[] args) {
+
+        Person person = new Person("t@esttest.net.de", "pP8437!Phcs");
+        Person person2 = new Person("t@esttest.net.de", "pP47!s");
+        Person person3 = new Person("test.net.de", "pP47!s");
+        Person person4 = new Person("t@esttestnetde", "pP8437!Phcs");
+        System.err.println("Только этот регистр работает, потому что в нем есть все критерие, которые были заданы: "+person);
+        System.out.println(person2);
+        System.out.println(person3);
+        System.out.println(person4);
+
+
+    }
+}
